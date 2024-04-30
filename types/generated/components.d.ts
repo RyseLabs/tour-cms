2fe2c6b542e4620571330d014cf2ef504e660042
ryan <ryan.seppel@jgeigerconsulting.com>
2024-04-30 14:39:08 -0500

    added tours

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,37 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ImagesImageGallery extends Schema.Component {
+  collectionName: 'components_images_image_galleries';
+  info: {
+    displayName: 'image-gallery';
+  };
+  attributes: {
+    title: Attribute.String;
+    description: Attribute.String;
+    image: Attribute.Media;
+  };
+}
+
+export interface PoisPointOfInterest extends Schema.Component {
+  collectionName: 'components_pois_point_of_interests';
+  info: {
+    displayName: 'point-of-interest';
+  };
+  attributes: {
+    title: Attribute.String;
+    description: Attribute.String;
+    cover: Attribute.Media;
+    latitude: Attribute.String;
+    longitude: Attribute.String;
+    images: Attribute.Component<'images.image-gallery', true>;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'images.image-gallery': ImagesImageGallery;
+      'pois.point-of-interest': PoisPointOfInterest;
+    }
+  }
 }
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -362,6 +362,32 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
+export interface ApiTourTour extends Schema.CollectionType {
+  collectionName: 'tours';
+  info: {
+    singularName: 'tour';
+    pluralName: 'tours';
+    displayName: 'tour';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    title: Attribute.String;
+    description: Attribute.String;
+    cover: Attribute.Media;
+    link: Attribute.String;
+    pois: Attribute.Component<'pois.point-of-interest', true>;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<'api::tour.tour', 'oneToOne', 'admin::user'> &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<'api::tour.tour', 'oneToOne', 'admin::user'> &
+      Attribute.Private;
+  };
+}
+
 export interface PluginUploadFile extends Schema.CollectionType {
   collectionName: 'files';
   info: {
@@ -798,6 +824,7 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
+      'api::tour.tour': ApiTourTour;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::content-releases.release': PluginContentReleasesRelease;
